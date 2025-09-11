7ede90114c70a8374df55d63e91c0449f11e84c8
Kartik Turak <kartikturak1@gmail.com>
2025-09-12 01:42:14 +0530

    user Page

--- actions/users.js
+++ actions/users.js
@@ -29,3 +29,36 @@ export async function updateUsername(username) {
 
   return { success: true, username };
 }
+
+
+// fetch user by username
+
+export async function getUserByUsername(username){
+  const user = await db.user.findUnique({
+    where:{username},
+    select:{
+      id:true,
+      name:true,
+      email:true,
+      imageUrl:true,
+      events:{
+          where:{isPrivate:false},
+          orderBy:{
+            createdAt:'desc'
+          },
+          select:{
+            id:true,
+            title:true,
+            description:true,
+            duration:true,
+            isPrivate:true,
+            _count:{
+              select:{bookings:true}
+            }
+          },
+      }
+    }
+  })
+
+  return user;
+}
---
+++ app/[username]/page.jsx
@@ -0,0 +1,42 @@
+import { getUserByUsername } from "@/actions/users";
+import EventCard from "@/components/event-card";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { notFound } from "next/navigation";
+import React from "react";
+
+const UserPage = async ({ params }) => {
+  const user = await getUserByUsername(params.username);
+
+  if (!user) {
+    notFound();
+  }
+
+  return (
+    <div className="container mx-auto px-4 py-4">
+      <div className="flex flex-col items-center mb-8 ">
+        <Avatar className="w-24 h-24 mb-4">
+          <AvatarImage src={user.imageUrl} alt={user.name}/>
+          <AvatarFallback>{user.name.charAt(0)}</AvatarFallback>
+        </Avatar>
+        <h1 className="text-2xl font-bold mb-2">{user.name}</h1>
+        <p className="text-gray-600 text-center">Welcome to my Scheduling Page. Please select an event below to book a call with me.</p>
+
+      </div>
+
+      {user.events.length === 0 ? (
+        <p className="text-center text-gray-600">No Public Events Available</p>
+      ) : (
+        <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+          {user.events.map((event)=> {
+            return (
+              <EventCard key={event.id} event={event} username={params.username}/>
+            )
+          })}
+        </div>
+      )}
+
+    </div>
+  );
+};
+
+export default UserPage;
