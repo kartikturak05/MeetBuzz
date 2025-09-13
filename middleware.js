c1dbb6cd5bcfa5d079edfe2918064ba9462e19a7
Kartik Turak <kartikturak1@gmail.com>
2025-09-13 20:01:23 +0530

    Middleware error fized

--- middleware.js
+++ middleware.js
@@ -1,23 +1,24 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher, redirectToSignIn } from '@clerk/nextjs/server';
 
 const isProtectedRoute = createRouteMatcher([
-    "/dashboard(.*)",
-    "/events(.*)",
-    "/meetings(.*)",
-    "/availability(.*)",
-])
+  "/dashboard(.*)",
+  "/events(.*)",
+  "/meetings(.*)",
+  "/availability(.*)",
+]);
 
-export default clerkMiddleware((auth,req)=>{
-    if(!auth().userId && isProtectedRoute(req)){
-        return auth().redirectToSignIn;
-    }
+export default clerkMiddleware((auth, req) => {
+  const { userId } = auth();
+  const path = req.nextUrl.pathname;
+
+  if (!userId && isProtectedRoute(path)) {
+    return redirectToSignIn({ returnBackUrl: req.url });
+  }
 });
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
     '/(api|trpc)(.*)',
   ],
 };
