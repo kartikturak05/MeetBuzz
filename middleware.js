3f2087dc44d12c12e78734ccf610aad901439752
Kartik Turak <kartikturak1@gmail.com>
2025-09-13 21:29:18 +0530

    middleware change

--- middleware.js
+++ middleware.js
@@ -1,24 +1,10 @@
-import { clerkMiddleware, createRouteMatcher, redirectToSignIn } from '@clerk/nextjs/server';
+// middleware.ts
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher([
-  "/dashboard(.*)",
-  "/events(.*)",
-  "/meetings(.*)",
-  "/availability(.*)",
-]);
-
-export default clerkMiddleware((auth, req) => {
-  const { userId } = auth();
-  const path = req.nextUrl.pathname;
-
-  if (!userId && isProtectedRoute(path)) {
-    return redirectToSignIn({ returnBackUrl: req.url });
-  }
-});
+export default clerkMiddleware();
 
 export const config = {
   matcher: [
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    '/(api|trpc)(.*)',
+    "/((?!_next|.*\\..*).*)", // protects all routes except static assets
   ],
 };
